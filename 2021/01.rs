580a68db5c6a6dac5aff59284a6ec56c7a6bc084
Clint Lawrence <clint.lawrence@gmail.com>
2021-12-01 20:28:21 +1100

    minor tweaks

--- 2021/01.rs
+++ 2021/01.rs
@@ -2,21 +2,21 @@ use std::fs;
 
 fn part_a(input: &String) -> i32 {
     let depths: Vec<i32> = input.lines().map(|x| x.parse().unwrap()).collect();
-    let pairs = depths[..(depths.len() - 1)].iter().zip(depths[1..].iter());
-    pairs.map(|(a, b)| if b > a { 1} else {0}).sum()
+    let pairs = depths.iter().zip(depths[1..].iter());
+    pairs.map(|(a, b)| if b > a { 1 } else { 0 }).sum()
 }
 
 fn part_b(input: &String) -> i32 {
     let depths: Vec<i32> = input.lines().map(|x| x.parse().unwrap()).collect();
     let ave_depths: Vec<i32> = depths.windows(3).map(|x| x.iter().sum()).collect();
-    let pairs = ave_depths[..(ave_depths.len()-1)].iter().zip(ave_depths[1..].iter());
-    pairs.map(|(a,b)| if b>a {1} else {0}).sum()
+    let pairs = ave_depths.iter().zip(ave_depths[1..].iter());
+    pairs.map(|(a, b)| if b > a { 1 } else { 0 }).sum()
 }
 
 fn main() {
-    let input: String = fs::read_to_string("01.txt")
-        .expect("Something went wrong reading the file");
-    
+    let input: String =
+        fs::read_to_string("01.txt").expect("Something went wrong reading the file");
+
     println!("Part A: {}", part_a(&input));
     println!("Part B: {}", part_b(&input));
 }
