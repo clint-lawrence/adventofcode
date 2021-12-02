7dae9df0f67bbbdccb1091a498ccf641f4009d07
Clint Lawrence <clint.lawrence@gmail.com>
2021-12-02 18:37:58 +1100

    don't need to average, just compare endpoints

--- 2021/01.rs
+++ 2021/01.rs
@@ -2,14 +2,13 @@ use std::fs;
 
 fn part_a(input: &String) -> i32 {
     let depths: Vec<i32> = input.lines().map(|x| x.parse().unwrap()).collect();
-    let pairs = depths.iter().zip(depths[1..].iter());
+    let pairs = depths.iter().zip(depths.iter().skip(1));
     pairs.map(|(a, b)| if b > a { 1 } else { 0 }).sum()
 }
 
 fn part_b(input: &String) -> i32 {
     let depths: Vec<i32> = input.lines().map(|x| x.parse().unwrap()).collect();
-    let ave_depths: Vec<i32> = depths.windows(3).map(|x| x.iter().sum()).collect();
-    let pairs = ave_depths.iter().zip(ave_depths[1..].iter());
+    let pairs = depths.iter().zip(depths.iter().skip(3));
     pairs.map(|(a, b)| if b > a { 1 } else { 0 }).sum()
 }
 
