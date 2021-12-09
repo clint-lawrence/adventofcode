3ce399f3d4243f8333239d87d63e4ec1cb6475d4
Clint Lawrence <clint.lawrence@gmail.com>
2021-12-10 07:50:17 +1100

    day 9, part 2

--- 2021/09.rs
+++ 2021/09.rs
@@ -36,8 +36,74 @@ fn part_a(input: &String) -> u32 {
     
 }
 
-fn part_b(input: &String) -> i32 {
-    0
+
+fn count_fill(map: &mut Vec<Vec<u32>>, x: usize, y: usize) -> u32 {
+    const FILL: u32 = 100;
+
+    let mut count = 0;
+
+    if map[y][x] == 9 || map[y][x] == FILL {
+        return 0;
+    }
+    map[y][x] = FILL;
+    count += 1;
+
+    if x > 0 {
+        count += count_fill(map, x - 1, y);
+    }
+    if x + 1 < map[0].len() {
+        count += count_fill(map, x + 1, y);
+    }
+    if y > 0 {
+        count += count_fill(map, x, y - 1);
+    }
+    if y +1 < map.len() {
+        count += count_fill(map, x, y + 1);
+    }
+    count
+}
+
+fn part_b(input: &String) -> u32 {
+    let mut map: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|chr| chr.to_digit(10).unwrap() as u32)
+                .collect()
+        })
+        .collect();
+    let size_y: isize = map.len() as isize;
+    let size_x: isize = map[0].len() as isize;
+    let mut minima: Vec<(usize, usize)> = Vec::new();
+
+    let lookup = |x: isize, y: isize| -> u32  {
+        if x < 0 || y < 0 || x >= size_x || y >= size_y {
+            // return the max hight, so local minimum on the edge are detected
+            return 9
+        }
+        map[y as usize][x as usize]
+    };
+    
+    for y in 0..size_y {
+        for x in 0..size_x {
+            let val = lookup(x, y);
+            if lookup(x-1, y) > val
+                && lookup(x+1, y) > val
+                && lookup(x, y-1) > val
+                && lookup(x, y+1) > val {
+                    minima.push((x as usize, y as usize));
+                }
+        }
+    }
+    
+    let mut fill_counts = minima
+        .iter()
+        .map(|(x, y)| count_fill(&mut map, *x, *y))
+        .collect::<Vec<u32>>();
+    fill_counts.sort();
+    fill_counts[(fill_counts.len()-3)..].iter().product()
+        
+    
 }
 
 fn main() {
