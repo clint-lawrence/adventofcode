85e7fc1c17e2203175e2c8694cf1b48083ba444a
Clint Lawrence <clint.lawrence@gmail.com>
2021-12-10 17:51:30 +1100

    day 10, part 2

--- 2021/10.rs
+++ 2021/10.rs
@@ -1,10 +1,9 @@
 use std::fs;
-use std::collections::HashMap;
 
 
 fn parse_line(line: &str) -> u32 {
     let mut parse_stack: Vec<char> = Vec::new();
-    for (i, c) in line.chars().enumerate() {
+    for c in line.chars().enumerate() {
         match c {
             '(' | '[' | '{' | '<' => parse_stack.push(c),
             ')' => if *parse_stack.last().unwrap() == '(' { parse_stack.pop(); } else { return 3; },
@@ -22,8 +21,36 @@ fn part_a(input: &String) -> u32 {
     input.lines().map(|line| parse_line(line)).sum()
 }
 
-fn part_b(input: &String) -> i32 {
-    0
+fn parse_lineb(line: &str) -> Option<u64> {
+    let mut parse_stack: Vec<char> = Vec::new();
+    for c in line.chars() {
+        match c {
+            '(' | '[' | '{' | '<' => parse_stack.push(c),
+            ')' => if *parse_stack.last().unwrap() == '(' { parse_stack.pop(); } else { return None; },
+            ']' => if *parse_stack.last().unwrap() == '[' { parse_stack.pop(); } else { return None; },
+            '}' => if *parse_stack.last().unwrap() == '{' { parse_stack.pop(); } else { return None; },
+            '>' => if *parse_stack.last().unwrap() == '<' { parse_stack.pop(); } else { return None; },
+            _ => panic!()
+        };
+    }
+    // either incomplete or well formed line
+    let mut acc = 0;
+    while let Some(c) = parse_stack.pop() {
+        acc = match c {
+            '(' => acc * 5 + 1,
+            '[' => acc * 5 + 2,
+            '{' => acc * 5 + 3,
+            '<' => acc * 5 + 4,
+            x => panic!("panic on char {}", x)
+        };
+    }
+    Some(acc)
+}
+
+fn part_b(input: &String) -> u64 {
+    let mut scores = input.lines().filter_map(|line| parse_lineb(line)).collect::<Vec<u64>>();
+    scores.sort();
+    scores[(scores.len()-1)/2]
 }
 
 fn main() {
